871e06295eb3970002c0d78c6b41b207de1fb254
Srinidhi Kaushik <shrinidhi.kaushik@gmail.com>
2019-05-28 22:46:40 +0530

    Redirect.

--- server.py
+++ server.py
@@ -4,12 +4,23 @@
 /dev/null -- Take what you can, give nothing back.
 '''
 
-from flask import Flask, request
+from flask import (Flask, request, url_for, redirect)
 
 APP = Flask(__name__)
+METHODS = [
+    'GET', 'HEAD', 'POST', 'PUT', 'DELETE',
+    'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'
+]
+
+@APP.errorhandler(404)
+def dev_null_redirect(err):
+    '''
+    Redirect everything to `/dev/null'.
+    '''
+    return redirect(url_for('serve'))
 
 
-@APP.route('/dev/null', methods=['GET', 'POST'])
+@APP.route('/dev/null', methods=METHODS)
 def serve():
     '''
     This is the default route. Doesn't do anything.
