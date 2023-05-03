00501b9d534343e5ff88bc5d7dc52f3b4b21e04b
Srinidhi Kaushik <shrinidhi.kaushik@gmail.com>
2023-05-03 10:10:25 -0400

    Placate depandabot.

--- server.py
+++ server.py
@@ -1,36 +1,43 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
-'''
-/dev/null -- Take what you can, give nothing back.
-'''
+"""
+/dev/null: Take what you can, give nothing back.
+"""
 
-from flask import (Flask, request, url_for, redirect)
+from flask import Flask, request, url_for, redirect
 
 APP = Flask(__name__)
 METHODS = [
-    'GET', 'HEAD', 'POST', 'PUT', 'DELETE',
-    'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'
+    "HEAD",
+    "GET",
+    "OPTIONS",
+    "TRACE",
+    "CONNECT",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
 ]
 
+
 @APP.errorhandler(404)
-def dev_null_redirect(err):
-    '''
+def dev_null_redirect(_):
+    """
     Redirect everything to `/dev/null'.
-    '''
-    return redirect(url_for('serve'))
+    """
+    return redirect(url_for("serve"))
 
 
-@APP.route('/dev/null', methods=METHODS)
+@APP.route("/dev/null", methods=METHODS)
 def serve():
-    '''
+    """
     This is the default route. Doesn't do anything.
-    '''
-    if request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
-        return ('', 200)
+    """
+    if request.method in ["POST", "PUT", "PATCH"]:
+        return ("", 204)
 
-    return ('', 204)
+    return ("", 200)
 
 
-
-if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=False)
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port=8080, debug=False)
